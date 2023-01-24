6c801309801aa7064d45014dd3a117db20b49642
Roopesh Acharya <rupeshacharya2014@gmail.com>
2023-01-25 00:21:49 +0545

    initalized project with constants

---
+++ const.py
@@ -0,0 +1,31 @@
+#dimensions for screen
+
+
+WIDTH = 800
+HEIGHT = 800
+
+
+#dimensions for board
+ROWS = 8
+COLS = 8
+SQSIZE = HEIGHT//ROWS # or WIDTH//COLS
+
+#colors
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+ORANGE = (255,165,0)
+PURPLE = (128,0,128)
+CYAN = (0,255,255)
+BROWN = (165,42,42)
+
+#colors for pieces
+PIECE_COLORS = [RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CYAN, BROWN]
+
+#dimensions for pieces
+RADIUS = SQSIZE//2 - 10
+
